f042fac34ab444a7adf8cddbe50172af41cc47d0
Aleksandr <check1984@gmail.com>
2025-07-01 01:08:47 +0500

    update processing1

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,11 @@
-from typing import List, Dict
+from typing import Dict, List
 
-def filter_by_state(operation_list: list[dict], state : str = "EXECUTED") -> list[dict] :
-    """
-    Фильтрует список словарей по значению ключа 'state'
-    """
-    return [transaction for transaction in operation_list if transaction.get("state") == state]
+
+def filter_by_state(info_list: List[Dict], state: str = "EXECUTED") -> List[Dict]:
+    """Функция для фильтрации словарей в списке по заданному ключу"""
+    return [item for item in info_list if item.get("state") == state]
+
+
+def sort_by_date(info_list: List[Dict], reverse: bool = True) -> List[Dict]:
+    """Функция сортировки списка словарей по дате"""
+    return sorted(info_list, key=lambda x: x["date"], reverse=True)
