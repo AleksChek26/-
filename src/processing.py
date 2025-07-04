26d6da4ff8f5d0fe4d3030bcac0f0bc191bbf685
Aleksandr <check198426@gmail.com>
2025-07-04 20:48:57 +0500

    Revert "homework 10_1"

--- src/processing.py
+++ src/processing.py
@@ -6,7 +6,6 @@ def filter_by_state(info_list: List[Dict], state: str = "EXECUTED") -> List[Dict
     return [item for item in info_list if item.get("state") == state]
 
 
-
 def sort_by_date(info_list: List[Dict], reverse: bool = True) -> List[Dict]:
     """Функция сортировки списка словарей по дате"""
     return sorted(info_list, key=lambda x: x["date"], reverse=True)
